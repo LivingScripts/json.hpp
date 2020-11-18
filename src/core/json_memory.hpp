b8a74641ac809d96dcb959b66902f9e68fc45683
livingscripts <nilesh22.a67@gmail.com>
2020-11-19 00:15:24 +0530

    syntax error fixes

--- src/core/json_memory.hpp
+++ src/core/json_memory.hpp
@@ -1,4 +1,5 @@
 #include <memory>
+#include <iostream>
 
 #include "json_type_traits.hpp"
 
@@ -8,14 +9,14 @@
 namespace json {
 // if type T does not have a clone method create a unique_ptr of it
     template<typename T,
-            typename std::enable_if<!json::cloneable<T>{}, bool>::value = true>
+            typename std::enable_if<!json::cloneable<T>{}, bool>::type = true>
     std::unique_ptr<T> clone(T const &t) {
         return std::make_unique<T>(t);
     }
 
 // if type T has clone method return a cloned copy of the object
     template<typename T,
-            typename std::enable_if<json::cloneable<T>{}, bool>::value = true>
+            typename std::enable_if<json::cloneable<T>{}, bool>::type = true>
     std::unique_ptr<T> clone(T const &t) {
         return t.clone();
     }
@@ -38,35 +39,32 @@ namespace json {
 
     // handling initialization via subclasses of T
         template<typename X,
-                typename std::enable_if<std::is_base_of<T, X>::value, bool> = true>
+                typename std::enable_if<std::is_base_of<T, X>::value, bool>::type = true>
         value_ptr(value_ptr<X> const &o):
                 base(o ? clone(*o) : nullptr)
         { }
 
     // template move constructor for subclasses of T
         template<typename X,
-                typename std::enable_if<std::is_base_of<T, X>::value, bool> = true>
+                typename std::enable_if<std::is_base_of<T, X>::value, bool>::type = true>
         value_ptr(value_ptr<X> &&o):
                 base(std::move(o))
         { }
-
 
     // initializing with a unique_ptr
         value_ptr(base b):
                 base(std::move(b))
         { }
 
-
     // copy constructor
         value_ptr(value_ptr const &o):
                 base(o ? clone(*o) : nullptr)
         { }
 
-
     // copy assignment
         value_ptr& operator=(value_ptr const &o) {
             if(!o) {
-                return this->reset();
+                this->reset();
             } else if(this != &o) {
                 auto tmp = clone(*o);
                 swap((base&)*this, tmp);
