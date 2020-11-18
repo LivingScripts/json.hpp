e41fca28ce73e1427b63d8483e7ca0ffc7b757f8
livingscripts <nilesh22.a67@gmail.com>
2020-11-19 00:44:35 +0530

    added namespace core

--- src/core/json_type_traits.hpp
+++ src/core/json_type_traits.hpp
@@ -4,17 +4,19 @@
 #define JSON_TYPE_TRAITS_HPP
 
 namespace json {
-/*
-* defaults to false type for any type T without a member named clone
-*/
-    template<typename T, class=void>
-    struct cloneable : std::false_type { };
+    namespace core {
+    /*
+    * defaults to false type for any type T without a member named clone
+    */
+        template<typename T, class=void>
+        struct cloneable : std::false_type { };
 
-/*
-* if type T has a method named clone, it is a true type
-*/
-    template<typename T>
-    struct cloneable<T, decltype(void(&T::clone))> : std::true_type { };
-}
+    /*
+    * if type T has a method named clone, it is a true type
+    */
+        template<typename T>
+        struct cloneable<T, decltype(void(&T::clone))> : std::true_type { };
+    } // core
+} // json
 
 #endif //JSON_TYPE_TRAITS_HPP
